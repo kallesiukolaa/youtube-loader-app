a005219c020dfec2fdaa501193236ce7c9574439
Kalle Siukola <kalle.siukola@pp.inet.fi>
2025-09-17 19:37:47 +0300

    Added folder creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ EFS_PATH_ENTIRE=$EFS_PATH/$OUTPUTFILE_SUFFIX
 
 echo Saving the file in the location $EFS_PATH_ENTIRE
 
+mkdir -p $EFS_PATH/$OUTPUTFILE_SUFFIX
+
 yt-dlp $YOUTUBE_URL --wait-for-video --live-from-start -o $EFS_PATH_ENTIRE
 
 echo Moving the output file from $EFS_PATH_ENTIRE to S3 location $S3_URI
