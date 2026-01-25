e410570d8d80de1485bd03a890821c90673720c9
Kalle Siukola <kalle.siukola@pp.inet.fi>
2026-01-25 10:24:05 +0200

    Added a flag to use ios and removed cookies.

--- entrypoint.sh
+++ entrypoint.sh
@@ -115,7 +115,7 @@ WATCHER_PID=$!
 # 6. video_part%03d.mp4  -> Naming pattern (part001, part002, etc.)
 
 yt-dlp "$YOUTUBE_URL" \
-    $COOKIE_ARGS \
+    --extractor-args "youtube:player_client=ios" \
     -S "proto:m3u8" \
     -f "best" \
     --output - \
